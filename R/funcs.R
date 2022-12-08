4ab686618b727cfdf413750b03a7adb198250273
fawda123 <mbafs2012@gmail.com>
2022-12-08 15:17:39 -0500

    gpra raw data to summaries

--- R/funcs.R
+++ R/funcs.R
@@ -1,11 +1,11 @@
 # hmpu database projects table
-rstdat_tab <- function(rstdat, yrrng, fntsz = 14, family){
+rstdat_tab <- function(dat, yrrng, fntsz = 14, family){
 
   if(length(yrrng) == 1)
     yrrng <- rep(yrrng, 2)
   
   # data prep
-  rstsum <- rstdat %>% 
+  rstsum <- dat %>% 
     select(
       Year = `Year Reported`, 
       Category = `Habitat Type (basic ESA categories)(existing databases)`, 
@@ -77,6 +77,87 @@ rstdat_tab <- function(rstdat, yrrng, fntsz = 14, family){
   
   # add title
   ttl <- paste0('Enhancement and restoration projects in Tampa Bay (', yrs, ')')
+  out <-  htmlwidgets::prependContent(tab, h5(class = "title", ttl))
+  
+  return(out)
+  
+}
+
+# gpra database projects table
+rstdat_tab2 <- function(dat, yrrng, fntsz = 14, family){
+
+  if(length(yrrng) == 1)
+    yrrng <- rep(yrrng, 2)
+  
+  # data prep
+  rstsum <- dat %>% 
+    select(
+      Year = `Federal Fiscal Year`, 
+      Category = `Habitat Type`, 
+      Acres, 
+      Activity = `Activity Name`, 
+      `Linear Miles` = `Miles`,
+      `Linear Ft` = `Feet`
+    ) %>% 
+    filter(!is.na(Activity)) %>% 
+    rowwise() %>% 
+    mutate(
+      Miles = sum(`Linear Miles`,  `Linear Ft` / 5280, na.rm = T)
+    ) %>% 
+    ungroup() %>% 
+    filter(Year <= yrrng[2] & Year >= yrrng[1]) %>% 
+    group_by(Category, Activity) %>% 
+    summarise(
+      tot= n(),
+      Acres = sum(Acres, na.rm = T), 
+      Miles = sum(Miles, na.rm = T),
+      .groups = 'drop'
+    ) %>% 
+    filter(!is.na(Category)) %>% 
+    group_by(Category) %>% 
+    mutate(
+      tot = sum(tot)
+    ) %>% 
+    pivot_longer(c('Acres', 'Miles'), names_to = 'var', values_to = 'val') %>% 
+    unite('var', Activity, var, sep = ', ') %>% 
+    pivot_wider(names_from = 'var', values_from = 'val', values_fill = 0) %>% 
+    select(Category, tot, `Restoration, Acres`, `Restoration, Miles`, `Maintenance, Acres`, `Maintenance, Miles`, `Protection, Acres`, `Protection, Miles`)
+  
+  # yrrng
+  yrs <- yrrng %>% 
+    unique %>% 
+    paste(., collapse = '-')
+  
+  # table
+  tab <- reactable(
+    rstsum, 
+    columns = list(
+      Category = colDef(name = 'Habitat', footer = 'Total',  minWidth = 200, class = 'sticky left-col-1-bord', headerClass = 'sticky left-col-1-bord', footerClass = 'sticky left-col-1-bord'), 
+      tot = colDef(name = 'Total projects', minWidth = 120)
+    ),
+    defaultColDef = colDef(
+      footer = function(values){
+        if(!is.numeric(values))
+          return()
+        
+        formatC(round(sum(values), 0), format= "d", big.mark = ",")
+        
+      },
+      headerStyle= list(fontSize = fntsz, fontFamily = family),
+      footerStyle = list(fontWeight = "bold", fontSize = fntsz, fontFamily = family),
+      format = colFormat(digits = 0, separators = TRUE), 
+      minWidth = 150,
+      style = list(fontSize = fntsz, fontFamily = family),
+      resizable = TRUE
+    ),
+    defaultPageSize = nrow(rstsum),
+    showPageSizeOptions = F,
+    highlight = T,
+    wrap = F
+  )
+  
+  # add title
+  ttl <- paste0('Restoration, maintenance, and protection projects in Tampa Bay (', yrs, ')')
   out <-  htmlwidgets::prependContent(tab, h5(class = "title", ttl))
   
   return(out)
--- R/tabs.R
+++ R/tabs.R
@@ -16,6 +16,8 @@ fml <- "roboto"
 
 source(here('R/funcs.R'))
 
+# HMPU compiled table -------------------------------------------------------------------------
+
 # https://docs.google.com/spreadsheets/d/1IkGidfU4SQJ_ZtOEqfn8licNCuWGlq_MtGuWWuIL1Js/edit#gid=2008440809
 rstdat <- read_sheet('1IkGidfU4SQJ_ZtOEqfn8licNCuWGlq_MtGuWWuIL1Js', sheet = 'FullDatabase')
 
@@ -25,3 +27,15 @@ tab2021 <- rstdat_tab(rstdat, yrrng = 2021, family = fml)
 
 save(taball, file = here('tabs/taball.RData'))
 save(tab2021, file = here('tabs/tab2021.RData'))
+
+# GPRA raw table ------------------------------------------------------------------------------
+
+# https://docs.google.com/spreadsheets/d/1QFX5XXpTgAr4u9wrxa_TUOuF5roUqEhJzZEDUiHi3SE/edit?usp=sharing
+rstdat2 <- read_sheet('1QFX5XXpTgAr4u9wrxa_TUOuF5roUqEhJzZEDUiHi3SE')
+
+taball_2 <- rstdat_tab2(rstdat2, yrrng = c(2006, 2022), family = fml)
+
+tab2022_2 <- rstdat_tab2(rstdat2, yrrng = 2022, family = fml)
+
+save(taball_2, file = here('tabs/taball_2.RData'))
+save(tab2022_2, file = here('tabs/tab2022_2.RData'))
