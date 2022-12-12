10343aa3400248c08792f11c2e75b68e5e035bfb
fawda123 <mbafs2012@gmail.com>
2022-12-12 15:21:42 -0500

    additional figures for recent years and 2022

--- R/figs.R
+++ R/figs.R
@@ -2,6 +2,9 @@ library(tidyverse)
 library(here)
 library(showtext)
 library(patchwork)
+library(plotrix)
+library(RColorBrewer)
+library(scales)
 
 load(file = here('data/rstdatall.RData'))
 
@@ -254,4 +257,168 @@ pout <- p1 + p2 + p3 + plot_layout(ncol = 1, guides = 'collect') & thm & guides(
 
 png(here('docs/figs/totalhmprecent.png'), height = 8, width = 7, family = 'serif', units = 'in', res = 500)
 print(pout)
+dev.off()
+
+# pie charts ----------------------------------------------------------------------------------
+
+levs <- c("Artificial Reefs", "Coastal Uplands", "Forested Freshwater Wetlands", 
+          "Hard Bottom", "Intertidal Estuarine (Other)", "Living Shorelines", 
+          "Low-Salinity Salt Marsh", "Mangrove Forests", "Non-forested Freshwater Wetlands", 
+          "Oyster Bars", "Salt Barrens", "Seagrasses", "Tidal Tributaries", "Uplands (Non-coastal)")
+cols <- colorRampPalette(brewer.pal(8, "Accent"))(length(levs))
+
+# data prep
+rstsum <- rstdatall %>% 
+  arrange(Year, Category) %>% 
+  filter(!is.na(Category)) %>%
+  filter(Year >= 2006) %>% 
+  filter(!is.na(Category)) %>% 
+  mutate(
+    Category = factor(Category, levels = levs)
+  ) %>% 
+  select(-Activity) %>% 
+  mutate(prj = 1) %>% 
+  tidyr::complete(Year, Category, fill = list(Acres = 0, Miles = 0, prj = 0)) %>% 
+  group_by(Year, Category) %>% 
+  summarise(
+    tot = sum(prj),
+    Acres = sum(Acres, na.rm = T),
+    Miles = sum(Miles, na.rm = T),
+    .groups = 'drop'
+  ) %>% 
+  group_by(Year) %>% 
+  mutate(
+    alltot = sum(tot),
+    allacres = sum(Acres), 
+    allmiles = sum(Miles)
+  ) 
+
+
+cols <- qualitative_hcl(length(unique(tnanndat$SOURCE)), palette = "Dynamic")
+
+toplo <- rstsum %>% 
+  filter(Year >= 2020) %>% 
+  mutate(
+    allacres2 = formatC(round(allacres, 1), format= "f", big.mark = ",", digits = 1), 
+    allmiles2 = formatC(round(allmiles, 1), format= "f", big.mark = ",", digits = 1)
+  ) %>% 
+  unite('Yeartot', Year, alltot, sep = ': ', remove = F) %>% 
+  unite('Yearacres', Year, allacres2, sep = ': ', remove = F) %>% 
+  unite('Yearmiles', Year, allmiles2, sep = ': ', remove = F)
+
+p <- ggplot(toplo, aes(x = alltot/2, y = tot, fill = Category, width = alltot)) +
+  geom_bar(position = "fill", stat="identity", color = 'black') +
+  facet_wrap(~ Yeartot, strip.position = 'bottom') + 
+  coord_polar("y") +
+  scale_fill_manual(values = cols) +
+  theme_void() +
+  theme(
+    legend.title = element_blank(), 
+    legend.position = 'top', 
+    strip.text = element_text(size = 12)
+    ) +
+  guides(fill = guide_legend(nrow = 5))
+
+png(here('docs/figs/totalpie.png'), height = 4, width = 7, family = 'serif', units = 'in', res = 500)
+print(p)
+dev.off()
+
+p <- ggplot(toplo, aes(x = allacres/2, y = Acres, fill = Category, width = allacres)) +
+  geom_bar(position = "fill", stat="identity", color = 'black') +
+  facet_wrap(~ Yearacres, strip.position = 'bottom') + 
+  coord_polar("y") +
+  scale_fill_manual(values = cols) +
+  theme_void() +
+  theme(
+    legend.title = element_blank(), 
+    legend.position = 'top', 
+    strip.text = element_text(size = 12)
+  ) +
+  guides(fill = guide_legend(nrow = 5))
+
+png(here('docs/figs/acrespie.png'), height = 4, width = 7, family = 'serif', units = 'in', res = 500)
+print(p)
+dev.off()
+
+p <- ggplot(toplo, aes(x = allmiles/2, y = Miles, fill = Category, width = allmiles)) +
+  geom_bar(position = "fill", stat="identity", color = 'black') +
+  facet_wrap(~ Yearmiles, strip.position = 'bottom') + 
+  coord_polar("y") +
+  scale_fill_manual(values = cols) +
+  theme_void() +
+  theme(
+    legend.title = element_blank(), 
+    legend.position = 'top', 
+    strip.text = element_text(size = 12)
+  ) +
+  guides(fill = guide_legend(nrow = 5))
+
+png(here('docs/figs/milespie.png'), height = 4, width = 7, family = 'serif', units = 'in', res = 500)
+print(p)
+dev.off()
+
+# 2022 only -----------------------------------------------------------------------------------
+
+toplo <- rstsum %>% 
+  filter(Year == 2022) %>% 
+  mutate(
+    Category = reorder(Category, tot),
+    acreslab = formatC(round(Acres, 1), big.mark = ",", format = 'f', digits = 1), 
+    acreslab = gsub('\\.0$', '', acreslab)
+  )
+
+thm <- theme_minimal() + 
+  theme(
+    panel.grid.minor = element_blank(), 
+    panel.grid.major.y = element_blank(), 
+    strip.placement = 'none', 
+    legend.position = 'none',
+    strip.background = element_blank(), 
+    strip.text.y = element_text(angle = 90, size = 10), 
+    strip.text.x = element_blank(),
+    axis.title.x = element_text(size = 11), 
+    panel.background = element_rect(fill = alpha('grey', 0.1), color = NA)
+  )
+
+p1 <- ggplot(toplo, aes(x = tot, y = Category, fill = Category)) + 
+  geom_bar(stat = 'identity') + 
+  geom_text(aes(label = tot), hjust = 0, nudge_x = 0.5) +
+  scale_x_continuous(expand = c(0, 0), limits = c(0, max(toplo$tot) * 1.18)) +
+  scale_fill_manual(values = cols) +
+  thm + 
+  labs(
+    y = NULL, 
+    x = 'Total projects'
+  )
+  
+p2 <- ggplot(toplo, aes(x = Acres, y = Category, fill = Category)) + 
+  geom_bar(stat = 'identity') + 
+  geom_text(aes(label = acreslab), hjust = 0, nudge_x = 1000) +
+  scale_x_continuous(expand = c(0, 0), limits = c(0, max(toplo$Acres) * 1.55), labels = comma) +
+  scale_fill_manual(values = cols) +
+  thm + 
+  theme(axis.text.y = element_blank()) +
+  labs(
+    y = NULL, 
+    x = 'Total Acres'
+  )
+
+p3 <- ggplot(toplo, aes(x = Miles, y = Category, fill = Category)) + 
+  geom_bar(stat = 'identity') + 
+  geom_text(aes(label = round(Miles, 2)), hjust = 0, nudge_x = 0.005) +
+  scale_x_continuous(expand = c(0, 0), limits = c(0, max(toplo$Miles) * 1.3)) +
+  scale_fill_manual(values = cols) +
+  thm + 
+  theme(axis.text.y = element_blank()) +
+  labs(
+    y = NULL, 
+    x = 'Total Miles'
+  )
+
+
+p <- p1 + p2 + p3 + plot_layout(ncol = 3)
+
+
+png(here('docs/figs/bar2022.png'), height = 5, width = 8, family = 'serif', units = 'in', res = 500)
+print(p)
 dev.off()
