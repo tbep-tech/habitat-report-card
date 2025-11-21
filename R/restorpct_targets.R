27fe00d9d959a21c8b029aaa486c29af96d929e3
fawda123 <mbafs2012@gmail.com>
2025-11-21 14:33:14 -0500

    complete 2030 restore trend est

--- R/restorpct_targets.R
+++ R/restorpct_targets.R
@@ -107,8 +107,7 @@ restore_pct <- restored %>%
   mutate(AcrestoGo=Target2030-(sum_acres+acres_LULC),
          MilestoGo=Target2030-sum_miles)
   
-
-############ stupid long version
+# alternative approach ---------------------------------------------------
 
 load(url('https://github.com/tbep-tech/hmpu-workflow/raw/refs/heads/master/data/acres.RData'))
 load(url('https://github.com/tbep-tech/hmpu-workflow/raw/refs/heads/master/data/subtacres.RData'))
@@ -119,6 +118,7 @@ trgshr <- trgs |>
 
 pth <- 'https://raw.githubusercontent.com/tbep-tech/TBEP_Habitat_Restoration/main/restoration.csv'
 
+# starting restoration data for 2017 (inter/supratidal) and 2018 (subtidal)
 strdat <- bind_rows(
     filter(acres, name == 2017),
     filter(subtacres, name == 2018)
@@ -134,59 +134,40 @@ strdat <- bind_rows(
   rename(start = Acres) |> 
   filter(HMPU_TARGETS %in% c(trgshr$HMPU_TARGETS))
 
-
-curdat <- read.csv(pth, stringsAsFactors = F) %>% 
+# currrent restoration data since 2017
+curdat <- read.csv(pth, stringsAsFactors = F) |> 
+  filter(GeneralActivity == 'Restoration' & Federal_Fiscal_Year >= 2017) |>
   select(
     Year = Federal_Fiscal_Year,
-    Partner = Lead_Implementer,
-    Lat = Latitude, 
-    Lon = Longitude,
-    Restoration_Technique,
-    Primary = PrimaryHabitat,
-    General = GeneralHabitat,
-    Activity = GeneralActivity,
+    HMPU_TARGETS = PrimaryHabitat,
     Acres,
     Miles,
     Feet
-  ) %>%
+  ) |>
   mutate(
-    Miles = case_when(
-      Activity %in% 'Maintenance' & Restoration_Technique %in% 'Debris Removal' ~ NA_real_,
-      T ~ Miles
-    ),
     Acres = as.numeric(Acres),
     Miles = case_when(
       is.na(Miles) & !is.na(Feet) ~ Feet / 5280,
       T ~ Miles
     ),
-    General = case_when(
-      General == 'estuarine' ~ 'Estuarine', 
-      grepl('^Upland', General) ~ 'Uplands',
-      grepl('^Mix|^Other', General) ~ 'Mixed', 
-      T ~ General
-    ), 
-    General = ifelse(General == '', NA, General),
-    Primary = ifelse(Primary == '', NA, Primary),
-    Activity = ifelse(Activity == '', NA, Activity)
-  ) %>% 
-  select(Year, Primary, Activity, Acres, Miles) |> 
-  filter(Activity == 'Restoration') |> 
-  filter(Year >= 2017 & Year <= 2025,
-         Activity=="Restoration")%>%
-  rename(HMPU_TARGETS=Primary)%>%
-  mutate(HMPU_TARGETS = case_when(
-    HMPU_TARGETS == "Low-Salinity Salt Marsh" ~ "Salt Marshes",
-    HMPU_TARGETS == "Non-forested Freshwater Wetlands" ~ "Non-Forested Freshwater Wetlands",
-    HMPU_TARGETS == "Uplands (Non-coastal)" ~ "Native Uplands",
-    HMPU_TARGETS == "Intertidal Estuarine (Other)" ~ "Total Intertidal",
-    TRUE ~ HMPU_TARGETS
-  )) |> 
+    HMPU_TARGETS = case_when(
+      HMPU_TARGETS == "Low-Salinity Salt Marsh" ~ "Salt Marshes",
+      HMPU_TARGETS == "Non-forested Freshwater Wetlands" ~ "Non-Forested Freshwater Wetlands",
+      HMPU_TARGETS == "Uplands (Non-coastal)" ~ "Native Uplands",
+      HMPU_TARGETS == "Intertidal Estuarine (Other)" ~ "Total Intertidal",
+      HMPU_TARGETS == '' ~ NA_character_,
+      TRUE ~ HMPU_TARGETS
+    )
+  ) |> 
   filter(HMPU_TARGETS != 'Total Intertidal') |> 
   summarise(
     Acres = sum(Acres, na.rm=TRUE),
     Miles = sum(Miles, na.rm=TRUE),
     .by=c(Year, HMPU_TARGETS)
-  ) |> 
+  ) 
+
+# current restoration progress based on 2017 starting for inter/supratidal, 2018 for subtidal
+prgdat <- curdat |> 
   tidyr::complete(Year, HMPU_TARGETS = strdat$HMPU_TARGETS, fill=list(Acres=0, Miles=0)) |> 
   mutate(
     progress = case_when(
@@ -204,5 +185,22 @@ curdat <- read.csv(pth, stringsAsFactors = F) %>%
   full_join(strdat, by="HMPU_TARGETS") |> 
   mutate(
     progress = progress + start
-  ) 
+  ) |> 
+  select(-start)
+
+# 2030 estimate based on last five years, likel to meet as T/F
+tnddat <- prgdat |> 
+  filter(Year <= max(Year) & Year > (max(Year) - 5)) |> 
+  nest(data = c(Year, progress)) |> 
+  mutate(
+    est2030 = purrr::map_dbl(data, ~{
+      mod <- lm(progress ~ Year, data = .x)
+      round(predict(mod, newdata = data.frame(Year = 2030)), 1)
+    })
+  ) |> 
+  unnest('est2030') |> 
+  left_join(trgshr, by="HMPU_TARGETS") |> 
+  mutate(
+    estmet = est2030 >= Target2030
+  )
 
